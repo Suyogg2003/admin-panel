7a919b7ad52cf68113996ffa082c2f2c8515656f
Suyogg2003 <edwansuyog@gmail.com>
2025-06-18 00:19:37 +0530

    added contact page with css and jsx

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,7 +1,90 @@
+import { useState } from "react";
+
 export const Contact = () => {
+  const [contact, setContact] = useState({
+    username: "",
+    email: "",
+    message: "",
+  });
+
+  // lets tackle out handelinput
+
+  const handleInput = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+
+    setContact({
+      ...contact,
+      [name]: value,
+    });
+  };
+
+  // handle form submit
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(contact);
+  };
   return (
     <>
-      <h1>Hello Contact page </h1>
+      <section className="section-contact">
+        <div className="contact-content container">
+          <h1 className="main-heading">contact us</h1>
+        </div>
+        {/*contact page main */}
+        <div className="container grid grid-two-cols">
+          <div className="contact-img">
+            <img src="/images/contact.png" alt="contact img" />
+          </div>
+
+          {/* actual contact form  */}
+          <section className="section-form">
+            <form onSubmit={handleSubmit}>
+              <div>
+                <label htmlFor="username">username</label>
+                <input
+                  type="text"
+                  name="username"
+                  id="username "
+                  autoComplete="off"
+                  value={contact.username}
+                  onChange={handleInput}
+                  required
+                />
+              </div>
+
+              <div>
+                <label htmlFor="email">email</label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email "
+                  autoComplete="off"
+                  value={contact.email}
+                  onChange={handleInput}
+                  required
+                />
+              </div>
+
+              <div>
+                <label htmlFor="message">message</label>
+                <textarea
+                  name="message"
+                  id="message"
+                  autoComplete="off"
+                  value={contact.message}
+                  onChange={handleInput}
+                  cols="30"
+                  rows="10"
+                ></textarea>
+              </div>
+
+              <div>
+                <button type="submit">submit</button>
+              </div>
+            </form>
+          </section>
+        </div>
+      </section>
     </>
   );
 };
