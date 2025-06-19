8df5e4d65046bec043c5b8a0aea06de35c045eb6
Suyogg2003 <edwansuyog@gmail.com>
2025-06-19 21:11:32 +0530

    created jsx and css file for analytics

---
+++ client/src/pages/Analytics.jsx
@@ -0,0 +1,31 @@
+import "../styles/analytics.css";
+
+export const Analytics = () => {
+  return (
+    <>
+      <section className="section-analytics">
+        <div className="contianer grid grid-four-cols">
+          <div className="div1">
+            <h2>50+</h2>
+            <p>registered companies</p>
+          </div>
+
+          <div className="div1">
+            <h2>100</h2>
+            <p>Happy clients</p>
+          </div>
+
+          <div className="div1">
+            <h2>500+</h2>
+            <p>well known Developers</p>
+          </div>
+
+          <div className="div1">
+            <h2>24/7</h2>
+            <p>Service</p>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
