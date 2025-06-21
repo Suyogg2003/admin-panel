7f244fe20b2c3d923ec2b3c0e60e3bde1843411a
Suyogg2003 <edwansuyog@gmail.com>
2025-06-21 23:40:34 +0530

    connected login page with backend

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,17 +1,34 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom"; // 👈 Import this , this hook is use for navigate from one page to another
 
 export const Login = () => {
   const [user, setUser] = useState({ email: "", password: "" });
+  const navigate = useNavigate(); // 👈 useNavigate hook
 
   const handleInput = (e) => {
     const { name, value } = e.target;
     setUser({ ...user, [name]: value });
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     console.log("Login Data:", user);
-    // Add login API call here
+
+    try {
+      const response = await fetch(`http://localhost:5000/api/auth/login`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(user), // converts obj into json
+      });
+      console.log(response);
+      if (response.ok) {
+        alert("login successful");
+        setUser[{ email: "", password: "" }];
+        navigate("/");
+      } else alert("invalid credentials");
+    } catch (error) {
+      console.log("login", error);
+    }
   };
 
   return (
