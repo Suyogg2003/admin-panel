44b2feb442e3444cc23a8e69a3192a57afab2419
Suyogg2003 <edwansuyog@gmail.com>
2025-06-22 12:42:10 +0530

    added hamburger menu functionality

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,36 +1,62 @@
 import { NavLink } from "react-router-dom";
+import { useState } from "react";
 import "./Navbar.css";
 
 export const Navbar = () => {
-  return (
-    <>
-      <header>
-        <div className="container">
-          <div className="logo-brand">
-            <NavLink href="/">Balaji Electronics</NavLink>
-          </div>
+  const [menuOpen, setMenuOpen] = useState(false);
 
-          <nav>
-            <ul>
-              <li>
-                <NavLink to="/">Home</NavLink>
-              </li>
-              <li>
-                <NavLink to="/about">About</NavLink>
-              </li>
-              <li>
-                <NavLink to="/contact">Contact</NavLink>
-              </li>
-              <li>
-                <NavLink to="/register">Register</NavLink>
-              </li>
-              <li>
-                <NavLink to="/login">Login</NavLink>
-              </li>
-            </ul>
-          </nav>
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <header>
+      <div className="container">
+        <div className="logo-brand">
+          <NavLink to="/">Balaji Electronics</NavLink>
         </div>
-      </header>
-    </>
+
+        {/* Hamburger Button */}
+        <div
+          className={`mobile-menu-toggle ${menuOpen ? "active" : ""}`}
+          onClick={toggleMenu}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+
+        {/* Navigation Menu */}
+        <nav className={menuOpen ? "active" : ""}>
+          <ul>
+            <li>
+              <NavLink to="/" onClick={() => setMenuOpen(false)}>
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/about" onClick={() => setMenuOpen(false)}>
+                About
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/contact" onClick={() => setMenuOpen(false)}>
+                Contact
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/register" onClick={() => setMenuOpen(false)}>
+                Register
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/login" onClick={() => setMenuOpen(false)}>
+                Login
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
   );
 };
